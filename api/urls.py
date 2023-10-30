75622084328646081b6f8039f6063c3fcc1b62b3
Dorothy2020 <wangaarraakoth@gmail.com>
2023-10-30 19:36:33 +0300

    feat:add viewsets

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
-from .views import article_list, article_detail,ArticleAPIView,ArticleDetails,GenericAPIView
+from django.urls import path,include
+from .views import article_list, article_detail,ArticleAPIView,ArticleViewSets,GenericAPIView
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register('article',ArticleViewSets, basename='article')
 
 urlpatterns=[
+    path('viewsets/',include(router.urls)),
+    path('viewsets/<int:pk>/',include(router.urls)),
     #path('article/',article_list),
     path('article/',ArticleAPIView.as_view()),
     #path('detail/<int:pk>/',article_detail),
-    path('detail/<int:id>/', ArticleDetails.as_view()),
-    path('generic/article/', GenericAPIView.as_view())
+    # path('detail/article/<int:id>/', ArticleDetailsAPIView.as_view()),
+    path('generic/article/<int:id>/', GenericAPIView.as_view())
 
 ]
